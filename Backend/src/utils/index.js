c63547edfdb30d178b512b7b4841f262d909bf38
ysumeet2004 <yadavtotorala99@gmail.com>
2025-08-24 11:15:54 +0530

    backend prog 1

---
+++ Backend/src/utils/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT;
+app.listen(PORT,()=>{
+    console.log(`Server connected to port ${PORT}`)
+});
